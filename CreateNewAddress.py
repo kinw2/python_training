d033cb68d2f7f2a6a5ee5940c0bafdb541b6d6e7
Waldek Kin <waldek.kin@vp.pl>
2022-09-06 23:42:33 +0200

    CreateNewAddress stupid version without uploading photo 2

--- CreateNewAddress.py
+++ CreateNewAddress.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 
 class CreateNewAddress(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Chrome('C:\Windows\SysWOW64\chromedriver')
-        self.wd.implicitly_wait(30)
+        self.wd.implicitly_wait(60)
 
 
     def test_create_new_address(self):
--- CreateNewGroup.py
+++ CreateNewGroup.py
@@ -2,7 +2,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 import unittest
 from group import Group
 
